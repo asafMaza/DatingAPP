ca5c940979ab5be0f256c6a231e329d9c98a4684
asafMaza <observur@gmail.com>
2022-02-21 12:06:34 +0200

    End of section 3

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { ThrowStmt } from '@angular/compiler';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'client';
+export class AppComponent implements OnInit{
+  title = 'Dating For You';
+  users: any;
+
+  constructor(private http: HttpClient){}
+
+  ngOnInit() {
+    this.getUsers();
+
+  }
+
+  getUsers(){
+    this.http.get('https://localhost:5001/api/users').subscribe(response => {
+      this.users = response;
+    }, error =>{
+      console.log(error);
+    })
+
+  }
 }
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,7 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+import {HttpClientModule } from '@angular/common/http'
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 
 @NgModule({
   declarations: [
@@ -10,9 +13,12 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+ 
